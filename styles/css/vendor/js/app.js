044a0779852abef8e52b2ad26dc6f9f4c68a7ee0
Surahman <surahman.duang@gmail.com>
2015-03-31 18:52:16 +0700

    Add detail page with magnifier effect on image

--- styles/css/vendor/js/app.js
+++ styles/css/vendor/js/app.js
@@ -5,3 +5,29 @@ $('.header--thumbnails img').mouseenter(function() {
 }).mouseleave(function() {
     $('header > div > img').attr('src', imgSource);
 });
+
+$('#zoom_1').elevateZoom({
+    gallery:'gal1',
+    zoomType: "inner",
+    responsive: true,
+    cursor: "crosshair",
+    galleryActiveClass: 'active',
+    zoomWindowFadeIn: 500,
+    zoomWindowFadeOut: 750,
+    loadingIcon: 'images/spinner.gif'
+});
+
+$("#zoom_1").bind("click", function(e) {
+    var ez = $('#zoom_1').data('elevateZoom');
+    $.fancybox(ez.getGalleryList());
+    return false;
+});
+
+$(window).bind('resize', function(e)
+{
+    if (window.RT) clearTimeout(window.RT);
+    window.RT = setTimeout(function()
+    {
+        this.location.reload(false); /* false to get page from cache */
+    }, 200);
+});
